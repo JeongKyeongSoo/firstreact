dc39795e87b333e5c125768fe0f52f92bcbfd962
unknown <coolmos@naver.com>
2020-03-12 23:21:53 +0900

    'movie'

--- README.md
+++ README.md
@@ -11,4 +11,16 @@ HTML을 반환하는 함수
     - unmoutning : componentWillUnMound
 
 
+##
+pm i react-router-dom
+
 https://www.youtube.com/watch?v=4horUrG9-Zw&list=PL7jH19IHhOLPp990qs8MbSsUlzKcTKuCf&index=20
+
+IIFE
+Modules
+
+## JS 초보
+https://www.youtube.com/watch?v=wUHncG3VwPw&list=PL7jH19IHhOLM8YwJMTa3UkXZN-LldYnyK
+
+## JS 핵심
+https://www.youtube.com/watch?v=QkFkFqg-J04&list=PL7jH19IHhOLMmmjrwCi7-dMFVdoU0hhgF&index=10
--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie({id, year, title, summary, poster}){
+function Movie({id, year, title, summary, poster, genres}){
   return (
     <div class='movies__movie'>
       <img src={poster} alt={title} titie={title}></img>
       <h3 class='movie__title' style={{backgroundColor: 'red'}}>{title}</h3>
       <h5 class='movie__year'>{year}</h5>
       <p class='movie__summary'>{summary}</p>
+      <ul className='genres'>
+        {genres.map((genre, index) => 
+          <li key={index} className="genres__genre">{genre}</li>
+        )}
+      </ul>
     </div>
     
   );
@@ -19,6 +24,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.string.isRequired,
 }
 
 export default Movie
